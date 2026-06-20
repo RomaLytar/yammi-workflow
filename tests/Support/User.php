b571fa32caa43b5246c1af81749203812cedc9a5
Roman Lytar <rlytar@ed3p.com>
2026-06-20 09:45:33 +0200

    Make the test user Notifiable so assignee notifications dispatch

--- tests/Support/User.php
+++ tests/Support/User.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace Yammi\Workflow\Tests\Support;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 /**
  * @property int $id
  */
 final class User extends Authenticatable
 {
+    use Notifiable;
+
     public $timestamps = false;
 
     protected $table = 'users';
